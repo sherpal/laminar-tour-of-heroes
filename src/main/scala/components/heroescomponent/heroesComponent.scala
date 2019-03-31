934c22c80bd02325d4f4b855533c0259619754d3
antoine <antoine.doeraene@b12-consulting.com>
2019-03-31 21:31:02 +0200

    Add the click event handler

--- src/main/scala/components/heroescomponent/heroesComponent.scala
+++ src/main/scala/components/heroescomponent/heroesComponent.scala
@@ -8,30 +8,19 @@ import org.scalajs.dom.html
 
 final case class heroesComponent() extends Component[html.Div] {
 
-  private val hero: Var[Hero] = Var(Hero(1, "Windstorm"))
+  private val selectedHero: Var[Hero] = Var(Hero(1, "Windstorm"))
 
   private val heroes: Var[List[Hero]] = Var(Heroes.heroes)
 
-  private val $heroesLI = heroes.signal.map(_.map(h => li(span(cls := "badge", h.id.toString), h.name)))
+  private val $heroesLI = heroes.signal.map(
+    _.map(h => li(
+      span(cls := "badge", h.id.toString), h.name,
+      onClick --> (_ => selectedHero.set(h))
+    ))
+  )
 
   private val styles = HeroesComponentStyles
 
-  /**
-    * <h2>My Heroes</h2>
-    * <ul class="heroes">
-    *   <li>
-    *     <span class="badge">{{hero.id}}</span> {{hero.name}}
-    *   </li>
-    * </ul>
-    * <h2>{{hero.name | uppercase}} Details</h2>
-    * <div><span>id: </span>{{hero.id}}</div>
-    * <div><span>name: </span>{{hero.name}}</div>
-    * <div>
-    *   <label>name:
-    *     <input [(ngModel)]="hero.name" placeholder="name"/>
-    *   </label>
-    * </div>
-    */
   val rel: ReactiveElement[html.Div] = div(
     h2("My Heroes"),
     ul(
@@ -39,16 +28,16 @@ final case class heroesComponent() extends Component[html.Div] {
       children <-- $heroesLI
     ),
     h2(
-      child <-- hero.signal.map(_.name.toUpperCase),
+      child <-- selectedHero.signal.map(_.name.toUpperCase),
       " Details"
     ),
-    div(span("id: "), hero.now().id.toString),
-    div(span("name: "), child <-- hero.signal.map(_.name)),
+    div(span("id: "), child <-- selectedHero.signal.map(_.id.toString)),
+    div(span("name: "), child <-- selectedHero.signal.map(_.name)),
     div(
       label("name:",
         input(
           placeholder := "name",
-          inContext(thisInput => onInput.mapTo(hero.now().copy(name = thisInput.ref.value)) --> hero.writer)
+          inContext(thisInput => onInput.mapTo(selectedHero.now().copy(name = thisInput.ref.value)) --> selectedHero.writer)
         )
       )
     )
